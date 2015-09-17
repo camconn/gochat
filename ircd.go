0701313fbab08f86fcc15e8a68d4c217cfc89e96
Cameron Conn <cam@camconn.cc>
2015-09-17 16:18:59 -0500

    basic network handling function

--- ircd.go
+++ ircd.go
@@ -45,6 +45,45 @@ type Packet struct {
 	Text   string
 }
 
+func NewClient(connection net.Conn) Client {
+	log.Println("New client: ", connection.RemoteAddr().String())
+	c := Client{
+		Conn: connection,
+	}
+
+	return c
+}
+
+func networkHandler() {
+	listener, err := net.Listen("tcp", ":6667")
+
+	if err != nil {
+		log.Fatal("Couldn't listen on port 6667: ", err)
+	}
+
+	msgsIn := make(chan string)
+	events := make(chan *Event)
+	// packets := make(chan *Packet)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Couldn't accept connection: ", err)
+		}
+
+		cl := NewClient(conn)
+		go handleConnection(&cl, msgsIn, events)
+	}
+}
+
+func handleConnection(cl *Client, in chan string, events <-chan *Event) {
+	return
+}
+
 func main() {
 	log.Println("Starting Server")
+
+	// TODO: Load configuration from file
+
+	networkHandler()
 }
